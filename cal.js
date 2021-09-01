3db291a61791463b0684eb7f269e9f21c2c3c1e2
Ayush Ambasta <ayushambasta2001@gmail.com>
2021-09-01 23:29:22 +0530

    back arrow button fixed

--- cal.js
+++ cal.js
@@ -3,7 +3,9 @@ var display=document.getElementById("display");
 var operand1=0;
 var operand2=null;
 var operator=null;
+var decimal=null;
 var result=null;
+var found=false; //to check decimal
 
 for(var i=0;i<buttons.length;i++){
     buttons[i].addEventListener('click',function(){
@@ -16,15 +18,24 @@ for(var i=0;i<buttons.length;i++){
             display.innerText=0;
         }else if(data=="back"){
             if(display.innerText=="NOT DEFINE"){
-                display.innerText="";
+                display.innerText=0;
             }
             var length=(display.innerText.length);
-            display.innerText=display.innerText.slice(0,length-1);
+            if(length==1){
+                display.innerText=0;
+            }else{
+                display.innerText=display.innerText.slice(0,length-1);
+            }
+            if(operand2!=null){
+                operand2=parseFloat(display.innerText);
+            }else{
+                operand1=parseFloat(display.innerText);
+            }
            
         }else if(data=="%" || data=="/" || data=="x" || data=="-"|| data=="+"){
             if(operand2==null){
-            operator=data;
-            display.innerText="";
+                operator=data;
+                display.innerText="";
             }
             else{
                 if(operator=="%"){
@@ -87,16 +98,17 @@ for(var i=0;i<buttons.length;i++){
                         operand2=null;
                         operand1=0;
                     }else{
-                    result=eval("operand1 / operand2");
-                    display.innerText=result;
-                    operand1=parseFloat(result);
-                    operand2=null;
-                    operator=null;
+                        result=eval("operand1 / operand2");
+                        display.innerText=result;
+                        operand1=parseFloat(result);
+                        operand2=null;
+                        operator=null;
                     }
                 }
                 if(operator=="x"){
                     result=eval("operand1 * operand2");
                     display.innerText=result;
+                    console.log(result);
                     operand1=parseFloat(result);
                     operand2=null;
                     operator=null;
@@ -121,20 +133,37 @@ for(var i=0;i<buttons.length;i++){
                 operator=null;
             }
         }
+        else if(data=="."){
+            found=false;
+            for(var j=0;j<display.innerText.length;j++){
+                if(display.innerText[j]=="."){
+                    found=true;
+                    break;
+                }
+            }
+            if(found==false){
+                if(display.innerText==0 ||display.innerText=="NOT DEFINE" || result!=null){
+                    display.innerText="";
+                 }
+                display.innerText+=data;
+            }
+        }
         else{
             if(operator==null){
-                if(display.innerText==0 || display.innerText=="NOT DEFINE" || result!=null){
+                if(display.innerText == 0 || display.innerText=="NOT DEFINE" || result!=null){
                     display.innerText="";
-                    result=null;""
+                    result=null;
                 }
                 display.innerText+=data;
                 operand1=parseFloat(display.innerText);
+                console.log(operand1);
             }else{
                 if(display.innerText==0){
                     display.innerText="";
                 }
                 display.innerText+=data;
                 operand2=parseFloat(display.innerText);
+                console.log(operand2);
             }
         } 
     });
